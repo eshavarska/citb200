29c959b84b3c46f0bd47ebc122218831298a0a7f
Elena Shavarska <e.shavarska@gmail.com>
2019-04-07 13:37:34 +0300

    aa

--- lab1/src/textprinter.cpp
+++ lab1/src/textprinter.cpp
@@ -6,7 +6,7 @@ using std::endl;
 
 void TextPrinter::print(std::ostream &out, Invoice invoice){
     out << std::fixed << std::setprecision(2);
-    
+
    for (auto item :  invoice.getItems()){
        out << item.getQuantity() << " x " << item.getProduct().getName() << " (" << item.getProduct().getPrice() << ")" 
        << " = " << invoice.computeItemTotal(item) << endl;
